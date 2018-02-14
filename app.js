9ec966db5e10b97326f429a9ebde3fa73d7381dd
Josh Kirby <jakirby@live.ca>
2018-02-14 17:18:56 +0000

    Init config

---
+++ app.js
@@ -0,0 +1,49 @@
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose"),
+    flash       = require("connect-flash"),
+    passport    = require("passport"),
+    LocalStrategy = require("passport-local"),
+    methodOverride = require("method-override"),
+    User        = require("./models/user");
+
+    
+//requiring routes
+var authRoutes = require("./routes/auth");
+var statusRoutes = require("./routes/status");
+var adminRoutes = require("./routes/admin");
+    
+mongoose.connect("mongodb://localhost/status");
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
+app.use(flash());
+
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "Josh Kirby Master of No Code",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+   res.locals.currentUser = req.user;
+   res.locals.error = req.flash("error");
+   res.locals.success = req.flash("success");
+   next();
+});
+
+app.use("/", authRoutes);
+app.use("/", statusRoutes);
+app.use("/", adminRoutes);
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Status Server Running");
+});
