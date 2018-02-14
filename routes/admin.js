d4ecdbee2f85a9e3e85d665e0fa2fe4ceca9919a
Josh Kirby <jakirby@live.ca>
2018-02-14 17:39:01 +0000

    Add /new route

--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,9 @@ var express = require("express");
 var router  = express.Router();
 var middleware = require("../middleware");
 
+//models
+var Status = require("../models/status");
+
 //root route
 router.get("/", function(req, res){
     res.render("index");
@@ -14,7 +17,23 @@ router.get("/admin", middleware.isLoggedIn, (req, res) => {
     res.render("admin/index");
 });
 
-
+router.post("/new", middleware.isLoggedIn, (req, res) => {
+    var type = req.body.type;
+    var message = req.body.message;
+    var author = {
+        id: req.user._id,
+        username: req.user.username
+    };
+    var newStatus = {type:type, message:message, author:author};
+    Status.create(newStatus, function(err, newlyCreated){
+        if(err){
+            throw(err);
+        } else {
+            //redirect back to new status page
+            res.redirect("/admin/status/new");
+        }
+    });
+});
 
 
 
